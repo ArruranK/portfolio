007e45f0f597951ad7aa207a355a3bf86b1bc032
ArruranK <arrurankamal@gmail.com>
2022-06-28 23:41:01 -0400

    window error fix attempt

--- src/hooks/useMousePosition.js
+++ src/hooks/useMousePosition.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 
 export const useMousePosition = () => {
-  
+  const isBrowser = typeof window !== "undefined";
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
-    window.addEventListener("mousemove", setFromEvent);
+    if (isBrowser) {
+      window.addEventListener("mousemove", setFromEvent);
+    }
+    
 
     return () => {
-      window.removeEventListener("mousemove", setFromEvent);
+      if (isBrowser) {
+        window.removeEventListener("mousemove", setFromEvent);
+      }
+      
     };
   }, []);
 
