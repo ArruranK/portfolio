20d580677f31180e1396c5c5aacee4303936c247
ArruranK <arrurankamal@gmail.com>
2022-07-07 11:28:31 -0400

    fixed gatsby build error

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
         module: {
           rules: [
             {
-              test: /bootstrap/,
+              test: /scrollreveal/,
               use: loaders.null(),
             },
           ],
